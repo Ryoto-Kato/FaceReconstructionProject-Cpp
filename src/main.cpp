2707cfa662e4ed63a3f31c91f7102ec7260701b5
bensturgis <ben.sturgis@web.de>
2023-06-15 12:16:53 +0200

    - Creation of folder structure for the project - Start of the creation of the code template At the moment the program reads the file data/bunny.off and then writes it back to a file result.off. To run the program, a lib folder must be created that contains the Eigen library. The file structure should be: 3Dmocap-FinalProject  - build/  - data/ 	bunny.off  - lib/ 	eigen-3.4.0  - include/ 	Eigen.h  - src/ 	main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,232 @@
+#include <iostream>
+#include <fstream>
+
+#include "../include/Eigen.h"
+
+struct Vertex {
+	Vector4f position;
+    Vector4uc color;
+};
+
+struct Triangle {
+	unsigned int idx0;
+	unsigned int idx1;
+	unsigned int idx2;
+
+	Triangle() : idx0{ 0 }, idx1{ 0 }, idx2{ 0 } {}
+
+	Triangle(unsigned int _idx0, unsigned int _idx1, unsigned int _idx2) :
+		idx0(_idx0), idx1(_idx1), idx2(_idx2) {}
+};
+
+class SimpleMesh {
+public:
+    std::vector<Vertex>& GetVertices() {
+		return m_vertices;
+	}
+
+	std::vector<Triangle>& GetTriangles() {
+		return m_triangles;
+	}
+
+	bool loadMesh(const std::string& filename) {
+		// Read off file (Important: Only .off files are supported).
+		m_vertices.clear();
+		m_triangles.clear();
+
+		std::ifstream file(filename);
+		if (!file.is_open()) {
+			std::cout << "Mesh file wasn't read successfully." << std::endl;
+			return false;
+		}
+
+		// First line should say 'COFF'.
+		char string1[5];
+		file >> string1;
+
+		// Read header.
+		unsigned int numV = 0;
+		unsigned int numP = 0;
+		unsigned int numE = 0;
+		file >> numV >> numP >> numE;
+
+		m_vertices.reserve(numV);
+		m_triangles.reserve(numP);
+
+		// Read vertices.
+		if (std::string(string1).compare("COFF") == 0) {
+			// We have color information.
+			for (unsigned int i = 0; i < numV; i++) {
+				Vertex v;
+				file >> v.position.x() >> v.position.y() >> v.position.z();
+				v.position.w() = 1.f;
+				// Colors are stored as integers. We need to convert them.
+				Vector4i colorInt;
+				file >> colorInt.x() >> colorInt.y() >> colorInt.z() >> colorInt.w();
+				v.color = Vector4uc((unsigned char)colorInt.x(), (unsigned char)colorInt.y(), (unsigned char)colorInt.z(), (unsigned char)colorInt.w());
+				m_vertices.push_back(v);
+			}
+		}
+		else if (std::string(string1).compare("OFF") == 0) {
+			// We only have vertex information.
+			for (unsigned int i = 0; i < numV; i++) {
+				Vertex v;
+				file >> v.position.x() >> v.position.y() >> v.position.z();
+				v.position.w() = 1.f;
+				v.color.x() = 0;
+				v.color.y() = 0;
+				v.color.z() = 0;
+				v.color.w() = 255;
+				m_vertices.push_back(v);
+			}
+		}
+		else {
+			std::cout << "Incorrect mesh file type." << std::endl;
+			return false;
+		}
+
+		// Read faces (i.e. triangles).
+		for (unsigned int i = 0; i < numP; i++) {
+			unsigned int num_vs;
+			file >> num_vs;
+			ASSERT(num_vs == 3 && "We can only read triangular mesh.");
+			
+			Triangle t;
+			file >> t.idx0 >> t.idx1 >> t.idx2;
+			m_triangles.push_back(t);
+		}
+
+		return true;
+	}
+	
+	bool writeMesh(const std::string& filename) {
+		// Write off file.
+		std::ofstream outFile(filename);
+		if (!outFile.is_open()) return false;
+
+		// Write header.
+		outFile << "COFF" << std::endl;
+		outFile << m_vertices.size() << " " << m_triangles.size() << " 0" << std::endl;
+
+		// Save vertices.
+		for (unsigned int i = 0; i < m_vertices.size(); i++) {
+			const auto& vertex = m_vertices[i];
+			if (vertex.position.allFinite())
+				outFile << vertex.position.x() << " " << vertex.position.y() << " " << vertex.position.z() << " "
+				<< int(vertex.color.x()) << " " << int(vertex.color.y()) << " " << int(vertex.color.z()) << " " << int(vertex.color.w()) << std::endl;
+			else
+				outFile << "0.0 0.0 0.0 0 0 0 0" << std::endl;
+		}
+
+		// Save faces.
+		for (unsigned int i = 0; i < m_triangles.size(); i++) {
+			outFile << "3 " << m_triangles[i].idx0 << " " << m_triangles[i].idx1 << " " << m_triangles[i].idx2 << std::endl;
+		}
+
+		// Close file.
+		outFile.close();
+
+		return true;
+	}
+
+private:
+	std::vector<Vertex> m_vertices;
+	std::vector<Triangle> m_triangles;
+};
+
+class BFM{
+public:
+    bool readBFM(const std::string& filename) {
+        return true;
+    }
+
+private:
+    SimpleMesh averageFace;
+    Eigen::MatrixXf principalShapeComponents;
+    std::vector<float> shapeVariance;
+    Eigen::MatrixXf principalTextureComponents;
+    std::vector<float> textureVariance;
+};
+
+class PointCloud {
+public:
+	std::vector<Eigen::Vector3f>& GetPoints() {
+		return m_points;
+	}
+
+	std::vector<Eigen::Vector3f>& GetNormals() {
+		return m_normals;
+	}
+
+private:
+	std::vector<Eigen::Vector3f> m_points;
+	std::vector<Eigen::Vector3f> m_normals;
+};
+
+class RGBImage {
+public:
+    RGBImage(int width, int height) : width_(width), height_(height) {
+        rgbData_.resize(width * height);
+    }
+
+    int getWidth() const {
+        return width_;
+    }
+
+    int getHeight() const {
+        return height_;
+    }
+
+    void setPixel(int x, int y, const Vector3uc& rgb) {
+        int index = y * width_ + x;
+        rgbData_[index] = rgb;
+    }
+
+    const Vector3uc& getPixel(int x, int y) const {
+        int index = y * width_ + x;
+        return rgbData_[index];
+    }
+
+private:
+    int width_;
+    int height_;
+    std::vector<Vector3uc> rgbData_;
+};
+
+class RGBDImage : public RGBImage {
+public:
+    RGBDImage(int width, int height) : RGBImage(width, height) {
+        depthData_.resize(width * height);
+    }
+
+    void setDepth(int x, int y, float depth) {
+        int index = y * getWidth() + x;
+        depthData_[index] = depth;
+    }
+
+    float getDepth(int x, int y) const {
+        int index = y * getWidth() + x;
+        return depthData_[index];
+    }
+
+private:
+    std::vector<float> depthData_;
+};
+
+int main() { 
+	const std::string filename = std::string("../Data/bunny.off");
+
+	SimpleMesh sourceMesh;
+	if (!sourceMesh.loadMesh(filename)) {
+		std::cout << "Mesh file wasn't read successfully at location: " << filename << std::endl;
+		return -1;
+	}
+
+	SimpleMesh resultingMesh = sourceMesh;
+	resultingMesh.writeMesh(std::string("result.off"));
+	std::cout << "Resulting mesh written." << std::endl;
+	
+	std::cout << "Lara <3" << std::endl;
+
+    return 0;
+}
